ebe32ed95e478f3e6f505c59c8c81143b74fbc60
datikken <tikken23@gmail.com>
2021-04-18 19:42:38 +0300

    test

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const bot = require('bot');
 
 const app = express()
 
+app.get('/',function(req,res) {
+    res.sendFile('index.html');
+});
+
 bot.launch()
 app.listen(PORT, () => console.log(`My server is running on port ${PORT}`))
